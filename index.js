4ff900ac786a4209efed8ade52329e4e6b7dd4bf
jue065257 <2139676637@qq.com>
2025-06-03 03:15:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello from wechat-daddy");
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello from wechat-daddy')
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
